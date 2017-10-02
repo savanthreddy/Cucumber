2c727a666103529e385fb9b1106125ca0de624d0
sravanth <sravanth@DESKTOP-CNL31VB>
2017-10-03 00:40:41 +0530

    TestNGCucumber

---
+++ src/main/java/admin/admin/TestRunner.java
@@ -0,0 +1,9 @@
+package admin.admin;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features= {"C:\\Users\\sravanth\\eclipse-workspace\\admin\\asd.feature"},glue= {})
+public class TestRunner extends AbstractTestNGCucumberTests {
+	
+}
